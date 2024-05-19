a52d76f9020126d3806c10a319749226fa6d4643
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-05-20 00:37:49 +0530

    UI formatting using prettier

--- whatsapp-clone-frontend/src/components/Whatsapp.jsx
+++ whatsapp-clone-frontend/src/components/Whatsapp.jsx
@@ -1,30 +1,26 @@
-import React from 'react'
-import Login from './Auth/Login'
-import {AppBar , Box, Toolbar , styled} from '@mui/material'
-
+import React from "react";
+import Login from "./Auth/Login";
+import { AppBar, Box, Toolbar, styled } from "@mui/material";
 
 const Header = styled(AppBar)`
-    height: 220px;
-    background-color:#00a884;
-    box-shadow:none;
-`
+  height: 220px;
+  background-color: #00a884;
+  box-shadow: none;
+`;
 
 const Component = styled(Box)`
-    height : 100vh;
-    background : #1f2223;
-
-`
+  height: 100vh;
+  background: #1f2223;
+`;
 const Whatsapp = () => {
   return (
     <Component>
-        <Header>
-            <Toolbar>
-
-            </Toolbar>
-        </Header>
-        <Login/>
+      <Header>
+        <Toolbar></Toolbar>
+      </Header>
+      <Login />
     </Component>
-  )
-}
+  );
+};
 
-export default Whatsapp
+export default Whatsapp;
