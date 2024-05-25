5a81cd68c4747ad673539d5eff5d2ab62a8a9b6b
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-05-26 01:17:56 +0530

    ThreeDot functionality done

--- whatsapp-clone-frontend/src/components/ChattingArea/MenuHeader.jsx
+++ whatsapp-clone-frontend/src/components/ChattingArea/MenuHeader.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import {Box, Typography} from '@mui/material'
 import AddCommentIcon from '@mui/icons-material/AddComment';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ThreeDot from './ThreeDot';
+
 
 const MenuHeader = () => {
   return (
     <div>
         <Box className="flex px-5 py-5">
             <Typography variant="h5" className="">Chats</Typography>
-            <Box className="ml-auto">
-                <AddCommentIcon className="mr-10"/>
-                <MoreVertIcon/>
+            <Box className="ml-auto flex">
+                <AddCommentIcon className="mr-10 text-gray-400"/>
+                <ThreeDot/>
             </Box>
         </Box>
     </div>
--- whatsapp-clone-frontend/src/components/ChattingArea/MenuSearchBar.jsx
+++ whatsapp-clone-frontend/src/components/ChattingArea/MenuSearchBar.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import { InputBase , Box } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+import { InputBase, Box } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const MenuSearchBar = () => {
   return (
     <>
-        <Box className="flex">
-            <Box className="flex item-center w-full mx-3 ">
-                
-                <SearchIcon  className="absolute text-3xl z-[80] mt-[11px] ml-3 py-[1px] text-[rgb(131,147,156)]" fontSize='small'/>
-                
-                <InputBase className='w-full bg-[#202c33] rounded-lg pl-16 py-1' placeholder='Search'/>
-            </Box>
-        </Box>
-    </>
-  )
-}
+      <Box className="flex">
+        <Box className="flex item-center w-full mx-3 ">
+          <SearchIcon
+            className="absolute text-3xl z-[80] mt-[11px] ml-3 py-[1px] text-[rgb(131,147,156)]"
+            fontSize="small"
+          />
 
-export default MenuSearchBar
+          <InputBase
+            className="w-full bg-[#202c33] text-[#7d8d97]  rounded-lg pl-16 py-1"
+            placeholder="Search"
+          />
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default MenuSearchBar;
