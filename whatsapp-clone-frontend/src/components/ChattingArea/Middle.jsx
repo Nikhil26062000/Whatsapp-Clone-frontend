b41b9dd17f8a52d433c9948ed6b76636fe45d943
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-05-26 00:45:26 +0530

    search bar and header component done

--- whatsapp-clone-frontend/src/components/ChattingArea/Middle.jsx
+++ whatsapp-clone-frontend/src/components/ChattingArea/Middle.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import MenuHeader from './MenuHeader'
+import MenuSearchBar from './MenuSearchBar'
 
 const Middle = () => {
   return (
     <div className='w-[31%] bg-[#111b21]'>
-
+      <MenuHeader/>
+      <MenuSearchBar/>
     </div>
   )
 }
