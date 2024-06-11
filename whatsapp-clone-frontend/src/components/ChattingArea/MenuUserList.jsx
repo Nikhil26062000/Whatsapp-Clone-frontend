ddeacfc204a047e902d107bfdf1ef19a85d14e0b
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-06-11 20:54:29 +0530

    Added search functionality in search field

--- whatsapp-clone-frontend/src/components/ChattingArea/MenuUserList.jsx
+++ whatsapp-clone-frontend/src/components/ChattingArea/MenuUserList.jsx
@@ -4,29 +4,33 @@ import { dummyDP } from "../../constants/data.js";
 import { useContext } from "react";
 import { AccountContext } from "../../context/accountProvider.jsx";
 
-
-const MenuUserList = () => {
+const MenuUserList = ({ filterUser }) => {
   const [userData, setUserData] = useState([]);
   const { account } = useContext(AccountContext);
-  const {setPersonDetails} = useContext(AccountContext);
-  const {personDetails} = useContext(AccountContext);
-  
+  const { setPersonDetails } = useContext(AccountContext);
+  const { personDetails } = useContext(AccountContext);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await getUser();
-      console.log(response.userData);
-      setUserData(response.userData);
+      // console.log(response.userData);
+      const filteredData = response?.userData?.filter((data) =>
+        data?.name?.toLowerCase().includes(filterUser?.toLowerCase())
+      );
+      setUserData(filteredData);
+      console.log(userData);
     };
 
     fetchData();
-  }, []);
+  }, [filterUser]);
 
-  const handlePersonDetails =async (userData) =>{
+  const handlePersonDetails = async (userData) => {
     setPersonDetails(userData);
     console.log(personDetails);
-    await setConversation({senderId:account.sub,receiverId:userData.sub})
-  }
+    
+    //! creating conversation between two user
+    await setConversation({ senderId: account.sub, receiverId: userData.sub });
+  };
   return (
     <div className="w-[100%] h-16 mt-2">
       {userData &&
@@ -35,7 +39,7 @@ const MenuUserList = () => {
             <div
               key={userData.sub}
               className="flex px-5 py-3 border-gray-800 border-b-2 border-t-0 border-l-0 border-r-0 hover:bg-[#222e35]"
-              onClick={()=>handlePersonDetails(userData)}
+              onClick={() => handlePersonDetails(userData)}
             >
               <img
                 src={userData.picture}
